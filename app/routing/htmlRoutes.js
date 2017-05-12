fcdef5358806d27db951a035675a1939b2b470e9
cesar avina <cesaravina@gmail.com>
2017-05-12 07:21:01 -0700

    jawsdb setup

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,20 +4,21 @@ var path = require("path");
 
 
 module.exports = function(app){
-	app.get('/survey', function(req, res){
-		res.sendFile(path.join(__dirname + '../public/survey.html'));
-		});
 
-	app.get('/', function(req, res){
-		res.sendFile(path.join(__dirname + '../public/home.html'));
-	});
+    app.get('/survey', function(req, res){
+        res.sendFile(path.join(__dirname + '/../public/survey.html'));
+    });
 
-	app.get('script.js', function(req, res){
-		res.sendFile(path.join(__dirname, '../public/script.js'))
-	});
+    app.get('/', function(req, res){
+        res.sendFile(path.join(__dirname + '/../public/home.html'));
+    });
 
-	app.get('/style.css', function(req, res){
-		res.sendFile(path.join(__dirname, '../public/style.css'))
-	});
+    app.get("/script.js", function(req, res) {
+        res.sendFile(path.join(__dirname, '/../public/script.js'));
+    });
+
+    app.get("/style.css", function(req, res) {
+        res.sendFile(path.join(__dirname, '/../public/style.css'));
+    });
+
 };
-
